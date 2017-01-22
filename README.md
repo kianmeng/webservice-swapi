e667289be745f1811acd96a49fbfd438847168d7
Kian Meng, Ang <kianmeng@cpan.org>
2017-01-22 18:11:57 +0800

    Add CPAN module structure

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# webservices-swapi
+# NAME
+
+Webservice::Swapi - It's new $module
+
+# SYNOPSIS
+
+    use Webservice::Swapi;
+
+# DESCRIPTION
+
+Webservice::Swapi is ...
+
+# LICENSE
+
+Copyright (C) Kian-Meng, Ang.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kian-Meng, Ang <kianmeng@users.noreply.github.com>
---
+++ lib/Webservice/Swapi.pm
@@ -0,0 +1,36 @@
+package Webservice::Swapi;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Webservice::Swapi - It's new $module
+
+=head1 SYNOPSIS
+
+    use Webservice::Swapi;
+
+=head1 DESCRIPTION
+
+Webservice::Swapi is ...
+
+=head1 LICENSE
+
+Copyright (C) Kian-Meng, Ang.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kian-Meng, Ang E<lt>kianmeng@users.noreply.github.comE<gt>
+
+=cut
